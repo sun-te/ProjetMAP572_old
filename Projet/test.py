966fdaed0f572f0ba8f00e4b28aea7b361a29df5
TeTe <32313299+sun-te@users.noreply.github.com>
2018-11-26 22:10:46 +0100

    20181126test

--- Projet/test.py
+++ Projet/test.py
@@ -8,24 +8,89 @@
 
 import numpy as np
 import os
-os.chdir("C:\\Users\\TeTe\\Desktop\\ProjetFinal")
+os.chdir("C:\\Users\\TeTe\\Desktop\\Projet")
 #import scipy as sp
 #from scipy.optimize import  minimize
-from TreePy import Cluster,Graph,Adjacency
+from TreePy import Cluster,Graph,Adjacency,PageRank
+from bokeh.io import  show
+from bokeh.plotting import figure
+from bokeh.layouts import column,row
+#%%
+'''
+Part1
+'''
+n=5000
+graph=Graph.grapheG()
+graph.reload(Adjacency.matrix_delta(n,0))
+Adj=graph.adjacence
+degree=np.sum(Adj,1)
+degree.sort()
+degrees=np.unique(degree)
+count_d=[]
+print("Construction complete")
+for i in degrees:
+    c=0
+    for m in range(len(degree)):
+        if(degree[m]==i):
+            c+=1
+    count_d+=[c]
+count_d=np.array(count_d)/n
+c=1.5
+fig=figure(x_axis_type="log",y_axis_type="log",plot_width=800,plot_height=800, y_range=[0.8/n,1],title='Recheche de alpha')
+fig.line(degrees,degrees**(-2)/c, line_width=2, legend="slope -2", color="red", line_dash='dotted')
+fig.line(degrees,degrees**(-2.2)/c, line_width=2, legend="slope -2.2", color="green", line_dash='dotted')
+fig.line(degrees,degrees**(-2.5)/c, line_width=2, legend="slope -2.2", color="orange", line_dash='dotted')
+fig.line(degrees,degrees**(-3)/c, line_width=2, legend="slope -3", color="blue", line_dash='dotted')
+fig.x(degrees, count_d,line_width=5)
+show(fig)
+#%%
 
-#%%    
+'''Part4'''
+M = np.array([[0,  0, 0, 0, 1],
+             [1, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [0,   1, 1, 0, 0],
+             [0,   0, 1, 1, 0]])
+
+
+
+M = np.array([[1,0,0,0,0,0,0,0,0,0,0],
+              [0,0,1,0,0,0,0,0,0,0,0],
+              [0,1,0,0,0,0,0,0,0,0,0],
+              [1,1,0,0,0,0,0,0,0,0,0],
+              [0,1,0,1,0,1,0,0,0,0,0],
+              [0,1,0,0,1,0,0,0,0,0,0],
+              [0,0,0,0,1,0,0,0,0,0,0],
+              [0,0,0,0,1,0,0,0,0,0,0],
+              [0,1,0,0,1,0,0,0,0,0,0],
+              [0,1,0,0,1,0,0,0,0,0,0],
+              [0,1,0,0,1,0,0,0,0,0,0]
+            ]).T
+
+
+graph=Graph.grapheG()
+graph.reload(M)
+graph.GPO_Armijio()
+graph.Visual(draw_edge=True)
+scores=PageRank.Scores(graph.adjacence,eps=0.15)
+print(scores)
+#%%   
 
 '''Part 3'''
 '''Question 3.1'''
 N =100
 graph=Graph.grapheG()
-graph.reload(Adjacency.matrix_delta(N,10))
+graph.reload(Adjacency.matrix_delta(N,1))
 graph.Visual()
 a=graph.Distance()
 graph.Energy(graph.pos)
-graph.GPO(itermax=800,tol=1.e-3)
+graph.GPO_d(itermax=800,tol=1.e-3)
 #%%
-k=3
+graph.GPO_Armijio(itermax=800,tol=1.e-3)
+#%%
+
+#%%
+k=4
 cluster_un=Cluster.Unnormalized(k,graph)
 cluster_n=Cluster.Normalized_sym(k,graph)
 #%%
@@ -40,7 +105,7 @@ graph.GPO(tol=0.01)
 k=3
 cluster_n=Cluster.Normalized_sym(k,graph)
 #%%
-n=200
+n=20
 K=3
 sommets=np.array(range(n))
 partition=np.zeros(n)
@@ -49,7 +114,7 @@ for i in range(n):
 tmp=np.random.uniform(0,0.5,[K,K])
 Q=tmp+tmp.T
 Q=np.array([[0.8,0.02,0.2],[0.02,0.8,0.1],[0.2,0.1,0.8]])
-eps=0.01
+eps=0.5
 Q=np.ones([K,K])*eps
 Q=Q+np.eye(K)*(1-eps)
 print(Q)
@@ -60,20 +125,6 @@ graph.GPO(itermax=200)
 #%%
 graph.VisualCluster(partition)
 cluster_n=Cluster.Normalized_sym(3,graph)
-
-#%%
-'''Part 4'''
-def Scores(A,eps,tol=1.e-6):
-    n=len(A)
-    Pe=(1-eps)*A+np.ones([n,n])*eps/n
-    tup=np.linalg.eig(Pe)
-    values=np.real(tup[0])
-    vectors=np.real(tup[1].T)
-    for i in range(len(values)):
-        if(np.abs(values[i]-1)<=tol):
-            index=i;
-    return vectors[index]
-
 
 
 
