b128af5dd1bcffd89bd03307248def0064e6c675
TeTe <32313299+sun-te@users.noreply.github.com>
2018-11-27 15:06:10 +0100

    Update test.py

--- Projet/test.py
+++ Projet/test.py
@@ -3,7 +3,7 @@
 '''
     test
     Created on Tue Nov 20 14:50:21 2018
-    @author: TeTe
+    @author: TeTe & Zeal
 '''
 
 import numpy as np
@@ -15,17 +15,20 @@ from TreePy import Cluster,Graph,Adjacency,PageRank
 from bokeh.io import  show
 from bokeh.plotting import figure
 from bokeh.layouts import column,row
+import pandas as pd
+import time
 #%%
 '''
 Part1
+Question1.2: P(degre de s=k) \approx ck^{-\alpha} 
 '''
 time0=time.time()
-n=1000
+n=100
 M=40
 dict_counter={}
 for i in range(M):
     graph=Graph.grapheG()
-    graph.reload(Adjacency.matrix_delta(n,0))
+    graph.reload(Adjacency.Matrix_delta(n,0))
     Adj=graph.adjacence
     degree=np.sum(Adj,1)
     degree.sort()
@@ -51,54 +54,27 @@ fig.x(degrees, count_d,line_width=5)
 show(fig)
 print("time: ", time.time()-time0)
 #%%
-
-'''Part4'''
-M = np.array([[0,  0, 0, 0, 1],
-             [1, 0, 0, 0, 0],
-             [1, 0, 0, 0, 0],
-             [0,   1, 1, 0, 0],
-             [0,   0, 1, 1, 0]])
-
-
-
-M = np.array([[1,0,0,0,0,0,0,0,0,0,0],
-              [0,0,1,0,0,0,0,0,0,0,0],
-              [0,1,0,0,0,0,0,0,0,0,0],
-              [1,1,0,0,0,0,0,0,0,0,0],
-              [0,1,0,1,0,1,0,0,0,0,0],
-              [0,1,0,0,1,0,0,0,0,0,0],
-              [0,0,0,0,1,0,0,0,0,0,0],
-              [0,0,0,0,1,0,0,0,0,0,0],
-              [0,1,0,0,1,0,0,0,0,0,0],
-              [0,1,0,0,1,0,0,0,0,0,0],
-              [0,1,0,0,1,0,0,0,0,0,0]
-            ]).T
-
+'''
+Partie2
+Question2.1-2.2: Optimisation de la representation du graphe par methode de gradient
+'''
+N =100
 graph=Graph.grapheG()
-graph.reload(M)
-scores=PageRank.Scores(graph.adjacence,eps=0.15)
-graph.reload(Adjacency.CompleteMatrix(M))
-graph.GPO_Armijio()
-graph.Visual(draw_edge=True)
-print(scores)
-
+graph.reload(Adjacency.Matrix_delta(N,1))
+graph.Visual()
+a=graph.Distance()
+graph.Energy(graph.pos)
+graph.GPO_d(itermax=800,tol=1.e-3)
+'''
+Question2.2 3d to do:
+'''
 #%%
-#4.2
-#test sur graph attachement preferentiel
-n=20
-graph=Graph.grapheG()
-graph.reload(Adjacency.matrix_delta(n,2.0))
-scores=PageRank.Scores(graph.adjacence,eps=0)
-graph.GPO_Armijio(itermax=200)
-graph.Visual(draw_edge=True)
-print(scores)
-#%%   
 
 '''Part 3'''
 '''Question 3.1'''
 N =100
 graph=Graph.grapheG()
-graph.reload(Adjacency.matrix_delta(N,1))
+graph.reload(Adjacency.Matrix_delta(N,1))
 graph.Visual()
 a=graph.Distance()
 graph.Energy(graph.pos)
@@ -113,17 +89,17 @@ cluster_un=Cluster.Unnormalized(k,graph)
 cluster_n=Cluster.Normalized_sym(k,graph)
 #%%
 '''Question 3.2'''
+'''teste sur l'exemple sur Moodle (N'excute pas avec impatience)'''
 MatriceAdjacence=np.loadtxt('StochasticBlockModel.txt')
 graph=Graph.grapheG()
 graph.reload(MatriceAdjacence)
 graph.Visual()
-#%%
-graph.GPO(tol=0.01)
-#%%
+graph.GPO_d(itermax=20,tol=0.01)
 k=3
 cluster_n=Cluster.Normalized_sym(k,graph)
 #%%
-n=20
+'''Construction une SVM, puis optimiser le graphe, et representer le clustering'''
+n=50
 K=3
 sommets=np.array(range(n))
 partition=np.zeros(n)
@@ -137,12 +113,40 @@ Q=np.ones([K,K])*eps
 Q=Q+np.eye(K)*(1-eps)
 print(Q)
 graph=Graph.grapheG()
-graph.reload(Adjacency.matrix_SBM(partition,Q))
+graph.reload(Adjacency.Matrix_SBM(partition,Q))
 #%%
-graph.GPO(itermax=200)
+graph.GPO_Armijio(itermax=200)                                            #Optimisation
 #%%
-graph.VisualCluster(partition)
-cluster_n=Cluster.Normalized_sym(3,graph)
+graph.VisualCluster(partition)                                            #CLustering Original
+cluster_n=Cluster.Normalized_sym(3,graph)                                 #Clustering detecter
+#%%
+
+'''Part4'''
+M = np.array([[0,  0, 0, 0, 1],
+             [1, 0, 0, 0, 0],
+             [1, 0, 0, 0, 0],
+             [0,   1, 1, 0, 0],
+             [0,   0, 1, 1, 0]])
+'''Question4.1: teste avec l'exemple de Wiki'''
+M = np.array([[1,0,0,0,0,0,0,0,0,0,0],
+              [0,0,1,0,0,0,0,0,0,0,0],
+              [0,1,0,0,0,0,0,0,0,0,0],
+              [1,1,0,0,0,0,0,0,0,0,0],
+              [0,1,0,1,0,1,0,0,0,0,0],
+              [0,1,0,0,1,0,0,0,0,0,0],
+              [0,0,0,0,1,0,0,0,0,0,0],
+              [0,0,0,0,1,0,0,0,0,0,0],
+              [0,1,0,0,1,0,0,0,0,0,0],
+              [0,1,0,0,1,0,0,0,0,0,0],
+              [0,1,0,0,1,0,0,0,0,0,0]
+            ]).T
+graph=Graph.grapheG()
+graph.reload(M)
+graph.GPO_Armijio()
+graph.Visual(draw_edge=True)
+scores=PageRank.Scores(graph.adjacence,eps=0.15)
+print(scores)
+#%%   
 
 
 
